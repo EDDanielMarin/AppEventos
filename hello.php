df9a0d30340454ecd0029c869b57e48a8189e81c
Daniel Marin <yop_dm@hotmail.com>
2017-02-16 18:01:35 -0500

    prueba

--- hello.php
+++ hello.php
@@ -7,3 +7,18 @@
  */
 
 echo "Hola mundo";
+?>
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Prueba de git</title>
+</head>
+<body>
+<h3>hola</h3>
+<h3>mundo</h3>
+<h3>hi</h3>
+<h3>world</h3>
+<h3>...</h3>
+</body>
+</html>
